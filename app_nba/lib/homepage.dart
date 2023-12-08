b46dcae6d067e412531076aa822d92cf1f118db7
mkswagger <mathangykrishna16@gmail.com>
2023-12-08 13:44:06 +0530

    modified the code

--- app_nba/lib/homepage.dart
+++ app_nba/lib/homepage.dart
@@ -4,55 +4,69 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:app_nba/model/team.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(teams: []),
+    );
+  }
+}
+
 class MyHomePage extends StatelessWidget {
   final List<Team> teams;
-  const MyHomePage({Key? key, required this.teams}) : super(key: key);
 
-  Future getTeams() async{
-    var response = await http.get(Uri.https('balldontlie.io','/api/v1/teams'));
+  MyHomePage({Key? key, required this.teams}) : super(key: key);
+
+  Future<List<Team>> getTeams() async {
+    var response = await http.get(Uri.https('balldontlie.io', '/api/v1/teams'));
     var jsonData = jsonDecode(response.body);
+    List<Team> teams = [];
 
-    for (var eachTeam in jsonData['data']){
-      final team = Team(abbreviation: eachTeam['abbreviation'] , 
-      city: eachTeam['city']
+    for (var eachTeam in jsonData['data']) {
+      final team = Team(
+        abbreviation: eachTeam['abbreviation'],
+        city: eachTeam['city'],
       );
       teams.add(team);
     }
-    // if (response.statusCode == 200) {
-    //   var jsonResponse = jsonDecode(response.body);
-    //   print(jsonResponse);
-    // } else {
-    //   print('Request failed with status: ${response.statusCode}.');
-    // }
+
     print(teams.length);
+    return teams;
   }
 
-  @override
+ @override
   Widget build(BuildContext context) {
-    getTeams();
     return Scaffold(
-      body: FutureBuilder(
+      body: FutureBuilder<List<Team>>(
         future: getTeams(),
-        builder:(context, snapshot) {
-          //is it done loading ? then show team data 
-          if(snapshot.connectionState == ConnectionState.done){
-                  return ListView.builder(
-                    itemCount: teams.length,
-                    itemBuilder:(context, index) {
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
 
-                    return ListTile(
-                      title: Text(teams[index].abbreviation),
-                    );
-                  },
-                  );
-          }
-          else{
-                 return const Center(
-                  child: CircularProgressIndicator(),
-                 );
+            final teams = snapshot.data ?? [];
+
+            return ListView.builder(
+              itemCount: teams.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(teams[index].abbreviation),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           }
         },
-        ),
+      ),
     );
   }
 }
