cb9521d753b1b775c90dbaa5e0d107204754a681
mkswagger <mathangykrishna16@gmail.com>
2023-12-08 13:17:15 +0530

    calling classes

--- app_nba/lib/homepage.dart
+++ app_nba/lib/homepage.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:ui' show lerpDouble;
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:app_nba/model/team.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
@@ -11,7 +12,8 @@ class MyHomePage extends StatelessWidget {
     var jsonData = jsonDecode(response.body);
 
     for (var eachTeam in jsonData['data']){
-      
+      final team = Team(abbreviation: eachTeam['abbreviation'] , 
+      city: eachTeam['city']);
     }
     // if (response.statusCode == 200) {
     //   var jsonResponse = jsonDecode(response.body);
