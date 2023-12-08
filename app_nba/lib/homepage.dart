494e51f1b1ee298c6ad57aebd95591dd9c5226b8
mkswagger <mathangykrishna16@gmail.com>
2023-12-08 13:04:10 +0530

    api response

--- app_nba/lib/homepage.dart
+++ app_nba/lib/homepage.dart
@@ -1,26 +1,19 @@
+import 'dart:convert';
 import 'dart:ui' show lerpDouble;
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   Future getTeams() async{
     var response = await http.get(Uri.https('balldontlie.io','/api/v1/teams'));
-    print(response.body);
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+      print(jsonResponse);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
   }
 
   @override
