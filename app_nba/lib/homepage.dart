2163903465a0c9d7eab555959cb920c14a422379
mkswagger <mathangykrishna16@gmail.com>
2023-12-08 13:34:56 +0530

    printing response into the app

--- app_nba/lib/homepage.dart
+++ app_nba/lib/homepage.dart
@@ -5,7 +5,8 @@ import 'package:http/http.dart' as http;
 import 'package:app_nba/model/team.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final List<Team> teams;
+  const MyHomePage({Key? key, required this.teams}) : super(key: key);
 
   Future getTeams() async{
     var response = await http.get(Uri.https('balldontlie.io','/api/v1/teams'));
@@ -13,7 +14,9 @@ class MyHomePage extends StatelessWidget {
 
     for (var eachTeam in jsonData['data']){
       final team = Team(abbreviation: eachTeam['abbreviation'] , 
-      city: eachTeam['city']);
+      city: eachTeam['city']
+      );
+      teams.add(team);
     }
     // if (response.statusCode == 200) {
     //   var jsonResponse = jsonDecode(response.body);
@@ -21,11 +24,35 @@ class MyHomePage extends StatelessWidget {
     // } else {
     //   print('Request failed with status: ${response.statusCode}.');
     // }
+    print(teams.length);
   }
 
   @override
   Widget build(BuildContext context) {
     getTeams();
-    return Scaffold();
+    return Scaffold(
+      body: FutureBuilder(
+        future: getTeams(),
+        builder:(context, snapshot) {
+          //is it done loading ? then show team data 
+          if(snapshot.connectionState == ConnectionState.done){
+                  return ListView.builder(
+                    itemCount: teams.length,
+                    itemBuilder:(context, index) {
+
+                    return ListTile(
+                      title: Text(teams[index].abbreviation),
+                    );
+                  },
+                  );
+          }
+          else{
+                 return const Center(
+                  child: CircularProgressIndicator(),
+                 );
+          }
+        },
+        ),
+    );
   }
 }
