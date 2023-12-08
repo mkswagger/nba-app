64e068026ff516d57060262d27ff273aaf0ee26e
mkswagger <mathangykrishna16@gmail.com>
2023-12-08 12:59:46 +0530

    fetching api response

--- app_nba/lib/homepage.dart
+++ app_nba/lib/homepage.dart
@@ -1,9 +1,31 @@
+import 'dart:ui' show lerpDouble;
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
+
+  Future getTeams() async{
+    var response = await http.get(Uri.https('balldontlie.io','/api/v1/teams'));
+    print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
+    getTeams();
     return Scaffold();
   }
 }
